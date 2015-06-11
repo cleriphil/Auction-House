78cb15d9c6788d1cdb3dd72b7d3ea503a6e8c432
cleri philiastides & Paul Ogawa <student@epicodus.com>
2015-06-11 16:06:06 -0700

    Add link to item on front page

--- controllers/lots.js
+++ controllers/lots.js
@@ -1,3 +1,12 @@
 AuctionHouse.LotsController = Ember.ArrayController.extend({
+  needs: ['items'],
 
+  // var itemsList = this.get('controllers.items.model')
+
+    // var lotsArray = this.get('model');
+    // var itemsArray = [];
+    // lotsArray.forEach(function(lot) {
+    //
+    //   itemsArray.push(lot.items)
+    // });
 });
